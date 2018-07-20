2d975f9b0a04b8a955b5e7a9e30fb8833f926ea9
Noah <noah@gangplankhq.com>
2018-07-20 10:04:22 -0700

    updated site

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$("#twitter-btn").on("click", function(e){
+    window.open("https://twitter.com/NeighborsNoah", "_blank");
+});
+
+$("#github-btn").on("click", function(e){
+    window.open("https://github.com/nneighbo", "_blank")
+});
+
+$("#linkedin-btn").on("click", function(e){
+    window.open("https://www.linkedin.com/in/noah-neighbors/", "_blank")
+});
